0d800dc9f46ced213aa1a9685f1f1d78655e1650
asmaasobhy <asmaasobhy1995@gmail.com>
2020-12-22 00:00:50 +0200

    question page style

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,25 +1,60 @@
 <template>
-    <div>
-        <div class="card" style="width: 18rem;">
+    <div class ='container my-5'>
+        <div class="card" style="width: 95%;">
             <div class="card-header">
-                Featured
+                {{this.questions[0]['question']}}
             </div>
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                    <label class="form-check-label" for="inlineRadio1">1</label>
+                <li class="list-group-item px-2">
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        {{this.questions[0]['correct_answer']}}
+                    </label>
                 </li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item px-2">
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        answer 2
+                    </label>
+                </li>
+                <li class="list-group-item px-2">
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        answer 3
+                    </label>
+                </li>
+                <li class="list-group-item px-2">
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        answer 4
+                    </label>
+                </li>
             </ul>
+            <div class="card-footer">
+                <button class="btn btn-dark mx-2" >prev</button>
+                <button class="btn btn-dark">next</button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
 export default {
-    name: "Question"
-}
+    name: "Question",
+    data(){
+        return{
+        questions:[]
+        }
+    },
+    created() {
+        axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple')
+        .then(res => this.questions = res.data.results)
+       
+        }
+        
+    }
 </script>
  
 <style scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,13 @@
 
 <script>
 
-import axios from 'axios'
-
 
 
 export default {
   name: 'Home',
   components: {
-  },
-  data(){
-    return{
-      questions:[]
-    }
-  },
-  created() {
-      //console.log("helloooooo")
-      axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple')
-      .then(res => console.log(this.questions=res.data.results))
-      //console.log(this.questions)
-    }
-  // methods: {
-    
-  // }
-}
+  }
+  }
 </script>
 
 <style>
