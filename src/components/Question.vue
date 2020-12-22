4a58629dc4fcf03f2d3d02ef941f66775b86df51
asmaasobhy <asmaasobhy1995@gmail.com>
2020-12-22 12:39:43 +0200

    array of answers displayed

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class ='container my-5'>
+    <div class ='container my-5' v-if="isLoaded">
         <div class="card" style="width: 95%;">
             <div class="card-header">
-                {{this.questions[0]['question']}}
+                {{this.questions[this.id]['question']}}
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item px-2">
-                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1">
                     <label class="form-check-label" for="exampleRadios1">
-                        {{this.questions[0]['correct_answer']}}
+                        {{this.answers[0]}}
                     </label>
                 </li>
                 <li class="list-group-item px-2">
-                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" >
                     <label class="form-check-label" for="exampleRadios1">
-                        answer 2
+                        {{this.answers[1]}}
                     </label>
                 </li>
                 <li class="list-group-item px-2">
-                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" >
                     <label class="form-check-label" for="exampleRadios1">
-                        answer 3
+                        {{this.answers[2]}}
                     </label>
                 </li>
                 <li class="list-group-item px-2">
-                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" checked>
+                    <input class="form-check-input mx-3" type="radio" name="exampleRadios" id="radio1" value="option1" >
                     <label class="form-check-label" for="exampleRadios1">
-                        answer 4
+                        {{this.answers[3]}}
                     </label>
                 </li>
             </ul>
@@ -40,18 +40,30 @@
 
 <script>
     import axios from 'axios'
-
 export default {
     name: "Question",
     data(){
         return{
-        questions:[]
+        questions:[],
+        id:1,
+        answers:[],
+        isLoaded:false
+
         }
+        
     },
     created() {
         axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple')
-        .then(res => this.questions = res.data.results)
-       
+        .then(res =>{
+        this.questions = res.data.results
+        this.id = location.pathname.split('/')[2]-1
+        this.answers=this.questions[this.id]['incorrect_answers']
+        this.answers.push(this.questions[this.id]['correct_answer'])
+        this.answers.sort()
+        this.isLoaded=true
+        console.log(this.answers)
+        console.log(this.questions[this.id]['correct_answer'])
+        })
         }
         
     }
