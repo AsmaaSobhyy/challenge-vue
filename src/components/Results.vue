dd0ff123877d5ad24cb4534ed474136db76ac894
asmaasobhy <asmaasobhy1995@gmail.com>
2020-12-22 15:25:31 +0200

    Results page created

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,26 @@
 <template>
-    <div>
-        result page
+    <div class = "container my-5"> 
+        <table class="table border">
+                
+            <tbody>
+                <tr class="text-muted py-5">
+                   <td colspan="4">Results</td>
+                </tr>
+                <tr class="table-active font-weight-light">
+                <td scope="col">The Question</td>
+                <td scope="col">Correct Answer</td>
+                <td scope="col">Your Answer</td>
+                <td scope="col">Points</td>
+                </tr>
+                <tr class ="text-dark">
+                <td scope="row">1</td>
+                <td >Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                
+            </tbody>
+            </table>
     </div>
 </template>
 
@@ -12,5 +32,4 @@ export default {
 </script>
  
 <style scoped>
-
 </style>
